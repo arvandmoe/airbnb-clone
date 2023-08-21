58ff0d617588c918c1aeb9f58e4143e7f5ff68aa
Mohammad Khaki Bakhtiarvand <mdkhaki10@gmail.com>
2023-08-21 20:58:02 +0330

    add ui lib

--- apps/web-next/app/page.tsx
+++ apps/web-next/app/page.tsx
@@ -1,3 +1,7 @@
+import { HelloServer } from '@airbnb-clone/ui/server'
+
 export default async function Index() {
-  return <div className="flex w-screen h-screen"></div>;
+  return <div className="flex w-screen h-screen">
+    <HelloServer></HelloServer>
+  </div>;
 }
